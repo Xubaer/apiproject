bca3a6d953854b391ebd66a8a5b32c1a4b6ac66c
Zubair Naseem <znaseem@csquareonline.com>
2018-03-20 21:34:16 +0500

    Second commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const logger = require('morgan');
+
+const app = express();
+
+const users = require('./routes/users');
+//Middlewares
+app.use(logger('dev'));
+
+//Routes
+app.use('/users', users);
+
+//Handle 404 error
+app.use((req, res, next) => {
+    const err = new Error("Not Found");
+    err.status = 404;
+    next(err);
+});
+
+//Error Handler
+app.use((err, req, res, next) => {
+    const error = app.get('env') == "development" ? err : {};
+    const status = error.status || 500;
+    //respond to client side
+    res.status(status).json({
+        error: {
+            message: error.message
+        }
+    })
+
+    //respond to ourselves
+    console.error(err);
+});
+
+//start server
+const port = app.get('port') || 3000;
+app.listen(port, () => console.log(`Server started Listening on port ${port}`) );
---
+++ routes/users.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const userController = require("../controllers/users");
+
+router.route('/')
+    .get(userController.index)
+    .post();
+
+module.exports = router;
